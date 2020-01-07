c3f3cb8934235acfc7b31234fc5eed019ac62743
Cristiano Souza <crstnsz@gmail.com>
2020-01-07 12:14:09 -0300

    Flush DNS

---
+++ sql/orarebusr.sql
@@ -0,0 +1,21 @@
+UNDEFINE USUARIO, ESPACO
+
+PROMPT &&USUARIO
+PROMPT &&ESPACO
+
+
+/* Excluir usuario */
+DROP USER &USUARIO. CASCADE;
+
+/* Criar usuario */
+CREATE USER &USUARIO. IDENTIFIED BY &USUARIO. DEFAULT TABLESPACE &ESPACO.;
+
+/* permite o usuario escrever no tablespace  */
+ALTER USER &USUARIO. QUOTA UNLIMITED ON &USUARIO.
+
+/* permite ao usuário conectar na instancia e criar objetos  */
+GRANT CONNECT, RESOURCE TO &USUARIO.
+
+/* informa que as duas regras são carregas com o usuário */
+ALTER USER &USUARIO. DEFAULT ROLE CONNECT, RESOURCE
+
