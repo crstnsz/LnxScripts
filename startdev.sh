15ac67860694100c0542e6638ea720d22b3be7ef
Cristiano Souza <cristianos@docspider.com.br>
2022-01-11 11:27:53 -0300

    Renomeando para ficar mais fácil de lembrar

--- startdev.sh
+++
@@ -1,5 +0,0 @@
-#VBoxManage startvm "Oracle 12C" --type headless &
-#VBoxManage startvm Desenvolvimento
-docker start oracle_container
-docker start elasticsearch_container
-docker start rabbitmq_container
